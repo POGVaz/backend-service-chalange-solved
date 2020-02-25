dc1ceedbe3bb553cb645795037b76382ee11dfd0
Pedro Oscar <pedro.oscar.vaz@usp.br>
2020-02-25 02:21:09 -0300

    Elaborated tests

--- postHandler/tests.py
+++ postHandler/tests.py
@@ -1,3 +1,98 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from unittest.mock import MagicMock
+from unittest.mock import patch
 
-# Create your tests here.
+from .models import User, Address, Answer
+from .tasks import shared_task
+
+example_post_OK = {
+	"user_info": {
+		"phone": "(11) 98765-4321",
+		"name": "João da Silva",
+		"email": "joao_silva@exemplo.com"
+	},
+	"address_attributes": {
+		"city": "São Paulo",
+		"neighborhood": "Jardim Paulista",
+		"street": "Avenida São Gabriel",
+		"uf": "SP",
+		"zip_code": "01435-001"
+	},
+	"request_info": {
+		"question1": "answer1",
+		"question2": "answer2",
+		"question3": "answer3"
+	}
+}
+
+class UserAddressViewTest(TestCase):
+
+	client = Client()
+
+	@patch("postHandler.add_user_address.fetchLocation.delay")
+	def test_post_OK_request(self, mock_location_fetcher_task):
+		'''
+		If a post request is made, save user, address and answer data to database
+		and create a celery task to retrieve that address location data.
+		'''
+
+		# Send the post request:
+		response = self.client.post(
+			reverse('postHandler:add_user_address'), example_post_OK)
+
+		# Check if the response is correct:
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "User information added.")
+
+		# Check if user was created in the database:
+		created_user = User.objects.get(name_text="João da Silva")
+		self.assertEqual(created_user.email_text, 'joao_silva@exemplo.com')
+		self.assertEqual(created_user.phone_text, '(11) 98765-4321')
+
+		#Check if address was created in the database:
+		created_address = created_user.address_set.get(city_text='São Paulo')
+		self.assertEqual(created_address.neighborhood_text, "Jardim Paulista")
+		self.assertEqual(created_address.street_text, "Avenida São Gabriel")
+		self.assertEqual(created_address.uf_code, "SP")
+		self.assertEqual(created_address.zip_code, "01435-001")
+
+		#Check if answers were created:
+		created_answer_1 = created_user.answer_set.get(answer_text='answer1')
+		self.assertEqual(created_answer_1.question_text, "question1")
+		created_answer_2 = created_user.answer_set.get(answer_text='answer2')
+		self.assertEqual(created_answer_2.question_text, "question2")
+		created_answer_3 = created_user.answer_set.get(answer_text='answer3')
+		self.assertEqual(created_answer_3.question_text, "question3")
+
+		#Check if location fetcher task was called:
+		mock_location_fetcher_task.assert_called_with("Avenida São Gabriel", "São Paulo", "SP")
+
+	# TODO: Add more tests, including exception occurring tests.
+		# IDEA: get request.
+		# IDEA: post request with missing info.
+		# IDEA: post request with wrong info (city that doesn't exist).
+		# IDEA: post request for already present user.
+
+class FetchLocationTaskTest(TestCase):
+
+
+	@patch('postHandler.tasks.google_client.geocode', MagicMock(return_value=[{"geometry": {"location": {"lat": 12.345678,"lng": 87.654321}}}]))
+	def test_fetch_example_location(self, mocked_geocode):
+		# Create address to update in the database
+		created_user = User(name_text="João da Silva")
+		created_user.save()
+		created_address = created_user.address_set.create(city_text="São Paulo")
+		created_address.save()
+
+		# Emit the task:
+		location_result = fetch_location(address_id=created_address.id, street="Avenida São Gabriel", city="São Paulo", state="SP")
+
+		# Check if the google API was called:
+		mocked_geocode.assert_called_with("Avenida São Gabriel, São Paulo, SP")
+
+		# Check if the address was updated:
+		created_address.refresh_from_db()
+		self.assertEqual(created_address.lat, 12.345678)
+		self.assertEqual(created_address.lat, 87.654321)
